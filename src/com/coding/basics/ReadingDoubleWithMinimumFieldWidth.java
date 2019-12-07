091b2f039c30cc55c06a0aa8d8622b1926fcd4d5
Amandeep Singh <aman21singh@gmail.com>
2019-12-07 18:38:52 +0530

    Adding program to read double with minimum field width

---
+++ src/com/coding/basics/ReadingDoubleWithMinimumFieldWidth.java
@@ -0,0 +1,18 @@
+package com.coding.basics;
+
+/*
+
+Question: Using Java, read in a double
+        (e.g. 1.4732, 15.324547327, etc.)
+        echo it, but with a minimum field width of 7 and 3 digits after the decimal point
+        (e.g. ss1.473 (where ‘s’ denotes a space), s15.325, etc.)
+*/
+import java.util.Scanner;
+
+public class ReadingDoubleWithMinimumFieldWidth {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Double value = sc.nextDouble();
+        System.out.printf("%7.3f", value);
+    }
+}
